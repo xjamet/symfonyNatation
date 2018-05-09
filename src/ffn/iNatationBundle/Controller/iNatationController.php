736cab339f78884803185b13c552ee1b800017a3
xilenz <xavier.jamet1@gmail.com>
2018-05-09 16:12:28 +0200

    ajout du bundle et du bootstrp

--- src/ffn/iNatationBundle/Controller/iNatationController.php
+++ src/ffn/iNatationBundle/Controller/iNatationController.php
@@ -6,4 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class iNatationController extends Controller
 {
+    public function accueilAction()
+    {
+        return $this->render('@ffniNatation/Default/accueil.html.twig');
+    }
 }
--- src/ffn/iNatationBundle/ffniNatationBundle.php
+++ src/ffn/iNatationBundle/ffniNatationBundle.php
@@ -7,8 +7,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ffniNatationBundle extends Bundle
 {
-    public function accueilAction()
-    {
-        return new Response("notre propre hello wor ! ");
-    }
+
 }
